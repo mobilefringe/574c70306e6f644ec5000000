d08f6cc8dd41ca25e11874bd54f3277a7b79e147
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-05-30 14:05:47 -0400

    Pico Rivera 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "The District at Green Valley Ranch",
+                    "default_image" : "http://assets.codecloudapp.com/sites/574c70306e6f644ec5000000/image/jpeg/1463773044000/prtc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "196558634"
+                }
