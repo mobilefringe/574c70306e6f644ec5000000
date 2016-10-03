ace06a6c228db9064c0a8f41574dfada4f2095e9
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-03 16:42:10 -0400

    Pico Rivera 1.1  /site.js

--- site.js
+++ site.js
@@ -2,6 +2,6 @@ var site_json = {
                     "name" : "Pico Rivera Towne Center",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f2a8246e6f645d03010000/image/jpeg/1463773044000/prtc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "http://pico.mallmaverick.com/api/v2/pico/social.json"
+                    "social_feed" : "//pico.mallmaverick.com/api/v2/pico/social.json"
                 }
             
